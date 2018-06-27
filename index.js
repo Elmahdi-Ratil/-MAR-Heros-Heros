610758e9faf10a7dfbe8cb6eebe4c39f780d8aea
heros4 <40445911+heros4@users.noreply.github.com>
2018-06-27 13:29:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -260,36 +260,4 @@ msg.author.send("Commands ستاتي " + `  **
 
 
 
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Mido King | 1help`,"https://www.twitch.tv/lboghaz_kingheros")
-  console.log('')
-  console.log('')
-  console.log('+[-----------------------------------------------------------------]+')
-  console.log(`[Start] ${new Date()}`);
-  console.log('+[-----------------------------------------------------------------]+')
-  console.log('')
-  console.log('+[------------------------------------]+');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('By The Atlantic Enjoy')
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('+[------------------------------------]+')
-  console.log('')
-  console.log('+[------------]+')
-  console.log(' Bot Is Online')
-  console.log('+[------------]+')
-  console.log('')
-  console.log('')
-});
-
-
-
-
 client.login(process.env.BOT_TOKEN);
