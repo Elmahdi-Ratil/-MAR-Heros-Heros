c91a8eb961e3c121677d4de8cd61587d27e84287
heros4 <40445911+heros4@users.noreply.github.com>
2018-06-27 05:30:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,74 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '!'
+const fs = require('fs');
+client.on("guildMemberAdd", member => {
+      
+      const welcomer = member.guild.channels.find("name","welcome"); //اسم روم
+                
+      var Canvas = require('canvas')
+      var jimp = require('jimp')
+      
+      const mi = ['./img/w1.png','./img/w2.png']; //يمكن ضيف '/img/w3.png','/img/w4.png'...
+      
+     
+      
+              let Image = Canvas.Image,
+                  canvas = new Canvas(401, 202),
+                  ctx = canvas.getContext('2d');
+              ctx.patternQuality = 'bilinear';
+              ctx.filter = 'bilinear';
+              ctx.antialias = 'subpixel';
+              ctx.shadowColor = 'rgba(0, 0, 0, 0.4)';
+              ctx.shadowOffsetY = 2;
+              ctx.shadowBlur = 2;
+              fs.readFile(`${mi[Math.floor(Math.random() * mi.length)]}`, function (err, Background) {
+                  if (err) return console.log(err)
+                  let BG = Canvas.Image;
+                  let ground = new Image;
+                  ground.src = Background;
+                  ctx.drawImage(ground, 0, 0, 401, 202);
+      
+      })
+      
+                     let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".png" : member.user.displayAvatarURL;
+                jimp.read(url, (err, ava) => {
+                    if (err) return console.log(err);
+                    ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
+                        if (err) return console.log(err);
+      
+                              //AVATARً
+                              let Avatar = Canvas.Image;
+                              let ava = new Avatar;
+                              ava.src = buf;
+                              ctx.drawImage(ava, 152, 27, 95, 95);
+                            //member number..
+                        ctx.font = '13px Arial';
+                        ctx.fontSize = '20px';
+                        ctx.fillStyle = "#FFFFFF";
+                        ctx.textAlign = "center";
+                        ctx.fillText(`انت العضو رقم  ${member.guild.memberCount} ! `, 340 , 98);
+    
+                                                      //name
+                              ctx.font = '20px Arial Bold';
+                              ctx.fontSize = '20px';
+                              ctx.fillStyle = "#FFFFFF";
+                              ctx.textAlign = "center";
+                                                         ctx.fillText(`${member.user.username}`, 200, 154);
+                             
+                             //server name
+                              ctx.font = '20px Arial';
+                              ctx.fontSize = '28px';
+                              ctx.fillStyle = "#FFFFFF";
+                              ctx.textAlign = "center";
+   ctx.fillText(` Welcome to ${member.guild.name} server` , 200, 190);
+   
+   
+
+    welcomer.sendFile(canvas.toBuffer())
+      
+      });
+      });
+      });
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -29,34 +97,6 @@ client.user.setGame(`Mido King |`,"https://www.twitch.tv/lboghaz_kingheros")
   console.log('')
   console.log('')
 });
- 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-LOka.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);
